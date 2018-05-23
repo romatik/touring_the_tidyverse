1b676f10d706920dc562d597f94537ddd763dc74
romatik <mikhail.balyasin@gmail.com>
2018-05-23 20:47:04 +0200

    splitting cells

---
+++ tidyr/split_cells.R
@@ -0,0 +1,52 @@
+# 4. Split cells
+# -, `uncount`,
+
+library(dplyr)
+library(tidyr)
+df <- data.frame(x = c(NA, "a.b", "a.d", "b.c"))
+df %>% separate(x, c("A", "B"))
+
+
+# If every row doesn't split into the same number of pieces, use
+# the extra and fill arguments to control what happens
+df <- data.frame(x = c("a", "a b", "a b c", NA))
+df %>% separate(x, c("a", "b"))
+
+# The same behaviour but no warnings
+df %>% separate(x, c("a", "b"), extra = "drop", fill = "right")
+# Another option:
+df %>% separate(x, c("a", "b"), extra = "merge", fill = "left")
+
+# If only want to split specified number of times use extra = "merge"
+df <- data.frame(x = c("x: 123", "y: error: 7"))
+df %>% separate(x, c("key", "value"), ": ", extra = "merge")
+
+# extract uses capturing groups to separate
+df <- data.frame(x = c(NA, "a-b", "a-d", "b-c", "d-e"))
+df %>% extract(x, "A")
+df %>% extract(x, c("A", "B"), "([[:alnum:]]+)-([[:alnum:]]+)")
+
+# If no match, NA:
+df %>% extract(x, c("A", "B"), "([a-d]+)-([a-d]+)")
+
+
+unite(mtcars, "vs_am", c("vs","am"))
+# remove removes input columns from the output
+unite(mtcars, "vs_am", c("vs","am"), remove = FALSE)
+
+### separate_rows
+df <- data.frame(
+  x = 1:3,
+  y = c("a", "d,e,f", "g,h"),
+  z = c("1", "2,3,4", "5,6"),
+  stringsAsFactors = FALSE
+)
+separate_rows(df, y, z, convert = TRUE)
+
+### uncount
+df <- tibble::tibble(x = c("a", "b"), n = c(1, 2))
+uncount(df, n)
+uncount(df, n, .id = "id")
+
+uncount(df, 2)
+uncount(df, 2 / n)
