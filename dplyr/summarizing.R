b55ffc6bf01b0db8c6b70aff16eef42c144730c4
romatik <mikhail.balyasin@gmail.com>
2018-07-19 21:43:41 +0200

    Adding context

--- dplyr/summarizing.R
+++ dplyr/summarizing.R
@@ -1,52 +1,52 @@
 library(tidyverse)
 
-###
+### group_by and count
 msleep %>%
   count(order, sort = TRUE)
 
-###
+### group_by multiple columns
 msleep %>%
   count(order, vore, sort = TRUE)
 
-###
+### count number of rows
 msleep %>%
   tally()
 
-###
+### add tally to all rows
 msleep %>%
   select(1:3) %>%
   add_tally()
 
-###
+### add count of specific column
 msleep %>%
   select(name:vore) %>%
   add_count(vore)
 
-###
+### summarize with multiple functions
 msleep %>%
   summarise(n = n(), average = mean(sleep_total), maximum = max(sleep_total))
 
-###
+### summarized group_by
 msleep %>%
   group_by(vore) %>%
   summarise(n = n(), average = mean(sleep_total), maximum = max(sleep_total))
 
-###
+### scoped summarize
 msleep %>%
   group_by(vore) %>%
   summarise_all(mean, na.rm = TRUE)
 
-###
+### using function
 msleep %>%
   group_by(vore) %>%
   summarise_all(~mean(., na.rm = TRUE) + 5)
 
-###
+### predicate
 msleep %>%
   group_by(vore) %>%
   summarise_if(is.numeric, mean, na.rm = TRUE)
 
-###
+### adding name of the function to the column name
 msleep %>%
   group_by(vore) %>%
   summarise_if(is.numeric, mean, na.rm = TRUE) %>%
@@ -68,7 +68,7 @@ msleep %>%
   summarise(avg_sleep = mean(sleep_total)) %>%
   arrange(desc(avg_sleep))
 
-###
+### arranging inside of each group
 msleep %>%
   select(order, name, sleep_total) %>%
   group_by(order) %>%
